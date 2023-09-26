b922d536a3a3f87ef636dc0e8298a25d97b25d59
cuso4lxt <2575862154@qq.com>
2023-09-26 19:05:28 +0800

    add the finished 3-6.py

--- homework/hw3/3-6.py
+++ homework/hw3/3-6.py
@@ -1,3 +1,15 @@
 # 用if语句实现百分制转等级制
-# 60分以下不合格，60-74为合格，75-89为优秀
+# 60分以下不合格，60-74为合格，75-89为良好，90分以上为优秀
 
+def grade_conversation(score):
+    if score < 60:
+        return '不合格'
+    elif score < 75:
+        return '合格'
+    elif score < 90:
+        return '良好'
+    else:
+        return '优秀'
+
+score = float(input())
+print(grade_conversation(score))
